3dae1ca0bfa9faf97e3057fb457d419b64bfe7bc
Cebukin <bruno.cebukin@wildlifestudios.com>
2019-12-01 18:29:08 -0300

    add image slideshow

---
+++ public/main.js
@@ -0,0 +1,51 @@
+window.onload = function() {
+    document.getElementById("open_audio").play();
+}
+
+$(document).ready(function() {
+  /*$('#previous').on('click', function(){
+    // Change to the previous image
+    $('#im_' + currentImage).stop().fadeOut(1);
+    decreaseImage();
+    $('#im_' + currentImage).stop().fadeIn(1);
+  }); 
+  $('#next').on('click', function(){
+    // Change to the next image
+    $('#im_' + currentImage).stop().fadeOut(1);
+    increaseImage();
+    $('#im_' + currentImage).stop().fadeIn(1);
+  }); */
+
+  var nextImage = function(){
+    // Change to the next image
+    $('#im_' + currentImage).stop().fadeOut(1);
+    increaseImage();
+    $('#im_' + currentImage).stop().fadeIn(1);
+  };
+
+  window.setInterval(function() {
+    nextImage();
+  }, 2500);
+
+  var currentImage = 1;
+  var totalImages = 7;
+
+  function increaseImage() {
+    /* Increase currentImage by 1.
+    * Resets to 1 if larger than totalImages
+    */
+    ++currentImage;
+    if(currentImage > totalImages) {
+      currentImage = 1;
+    }
+  }
+  function decreaseImage() {
+    /* Decrease currentImage by 1.
+    * Resets to totalImages if smaller than 1
+    */
+    --currentImage;
+    if(currentImage < 1) {
+      currentImage = totalImages;
+    }
+  }
+});
